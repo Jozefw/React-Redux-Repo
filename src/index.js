fbc61c973b37e8e6baca6d0faf7bd5617f027244
Jozef <jozefw@sbcglobal.net>
2017-10-21 14:22:47 -0700

    refactor from functional to Class based search bar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@
 import React from 'react';
 // react-dom places components into the dom
 import ReactDOM from 'react-dom';
-// Make a new component that renders html and then is inserted into the dom
-
+// files that we create need to be given a relative path
+import SearchBar from './components/search_bar';
+const API_KEY = "AIzaSyCOPF0Mc76x79YcZlmOBv0955Bjg3oEHLA";
 // App is a class, we need to instantiate our class by wrapping it in tags (self closing is JSX) so <app></app> or <app /> insantiates APP
 const App = function() {
-	return <div>Hola!</div>
+	return (<div>
+		<SearchBar />
+	</div>)
 }
 
 // second argument tells us where to render it
