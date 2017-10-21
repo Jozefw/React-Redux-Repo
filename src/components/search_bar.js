8f4d089bc27cde1b803d6112f81f8d5c0437db5d
Jozef <jozefw@sbcglobal.net>
2017-10-21 14:46:54 -0700

    add new methods'

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const SearchBar = () =>{
-	return <input />
+
+// rewrite from functional based componenet to a class based component which is more self aware
+// const SearchBar = () =>{
+// 	return <input />
+// }
+
+class SearchBar extends React.Component{
+	// add render function es6 style
+	render(){
+		// if we dont return som jsx it will barf
+		return <input />;
+	}
+	onInputChange(){
+		
+	}
+
 }
 
 export default SearchBar;
